63b3b1f57ac504aa57f78ac9a8968ff8975d5036
kurohikari123 <patrickkharmujai123@gmail.com>
2025-11-05 10:33:14 +0530

    Open weather API integration

--- server/index.js
+++ server/index.js
@@ -1,5 +1,34 @@
 import express from "express";
+import cors from "cors";
+import axios from "axios";
+import "dotenv/config";
 const app = express();
+
+let open_weather_api_key = process.env.OPEN_WEATHER_API;
+
+//All middleware here
+app.use(cors());
+app.use(express.json());
+
+//Get city weather
+app.get("/api/weather", async (req, res) => {
+  const { city } = req.query;
+
+  if (!city) {
+    res.status(400).json({ error: "City not found!" });
+  }
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${open_weather_api_key}&units=metric`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    const weatherData = response.data;
+    res.json(weatherData); //All weather data
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not find complete request" });
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server started in http://localhost:3000");
