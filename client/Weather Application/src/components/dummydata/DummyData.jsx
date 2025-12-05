aab8133efaa7cf1dbfe340aa8c3a11cb7ecf3db9
kuraohikari123 <patrick@patrick.localdomain>
2025-12-05 23:42:40 +0530

    removed dummy data

--- client/Weather Application/src/components/dummydata/DummyData.jsx
+++ client/Weather Application/src/components/dummydata/DummyData.jsx
@@ -1,17 +1,17 @@
-import { ClearSkyIcon, CloudIcon, WavesIcon } from "../icons/WeatherIcons";
-
-const forecastData = [
-  { day: "TODAY", temp: 21, Icon: ClearSkyIcon, condition: "Clear Sky" },
-  { day: "TUE", temp: 13, Icon: CloudIcon, condition: "Cloudy" },
-  { day: "WED", temp: 16, Icon: WavesIcon, condition: "Windy" },
-  { day: "THU", temp: 20, Icon: CloudIcon, condition: "Partly Cloudy" },
-  { day: "FRI", temp: 21, Icon: ClearSkyIcon, condition: "Sunny" },
-  { day: "SAT", temp: 21, Icon: ClearSkyIcon, condition: "Sunny" },
-];
-
+// import { ClearSkyIcon, CloudIcon, WavesIcon } from "../icons/WeatherIcons";
+//
+// const forecastData = [
+//   { day: "TODAY", temp: 21, Icon: ClearSkyIcon, condition: "Clear Sky" },
+//   { day: "TUE", temp: 13, Icon: CloudIcon, condition: "Cloudy" },
+//   { day: "WED", temp: 16, Icon: WavesIcon, condition: "Windy" },
+//   { day: "THU", temp: 20, Icon: CloudIcon, condition: "Partly Cloudy" },
+//   { day: "FRI", temp: 21, Icon: ClearSkyIcon, condition: "Sunny" },
+//   { day: "SAT", temp: 21, Icon: ClearSkyIcon, condition: "Sunny" },
+// ];
+//
 const scheduleData = [
   { time: "16:30h", event: "Stay at Bohém Art Hotel" },
   { time: "18:30h", event: "Dinner at Cafe Luna" },
 ];
 
-export { forecastData, scheduleData };
+export {scheduleData };
