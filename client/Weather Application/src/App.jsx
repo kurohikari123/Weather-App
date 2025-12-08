952fe3dbd8556b1cb627fc801e1fd176bb9f8f2e
kuraohikari123 <patrick@patrick.localdomain>
2025-12-08 22:01:41 +0530

    Added react router

--- client/Weather Application/src/App.jsx
+++ client/Weather Application/src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import WeatherPage from "./components/WeatherDashboard";
+import Login from "./components/Login.jsx"
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <WeatherPage />
-    </>
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+
+         {/* Need to validate before navigating directly to the dashboard */}
+        <Route path="/dashboard" element={<WeatherPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
