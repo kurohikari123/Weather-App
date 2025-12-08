944de8f605a2a29e1f8a3e2b77ca914ff01313f4
kuraohikari123 <patrick@patrick.localdomain>
2025-12-08 23:47:30 +0530

    Added login route

---
+++ server/routes/user.js
@@ -0,0 +1,44 @@
+//Defines all routes
+import axios from 'axios'
+import express from "express"
+import pool from '../database/db.js'
+
+const router = express.Router()
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+//Login Route
+router.post('/login',async (req,res)=>{
+
+  const email = req.body.email
+  const password = req.body.password
+
+  try{   
+
+  //Validate email and password before posting
+  if(!emailRegex.test(email)){
+    res.status(400).json({"status":0,"message":"Invalid email"})
+  }
+
+  const sql = 'select password from user where email = ? '
+  const row = await pool.query(sql,[email])
+  console.log(row[0][0])
+
+  //Check if user exist or not
+  if(row[0].length == 0){
+    res.json({"status":2,"message":"Invalid email or password"})
+  }
+
+  //Test password
+  if(row[0][0].password == password){
+    res.status(200).json({"status":1,"message":"Login Successful"})
+  }
+
+  res.json({"status":4,"message":"Invalid email or password"})
+
+  }catch(e){
+   console.log(e.message)
+   res.json({"status":4,"message":"Login Failed"})
+  }
+})
+
+export default router
