51a41619dbfb8b843743e779952363a1eda354f1
kuraohikari123 <patrick@patrick.localdomain>
2025-12-15 22:47:38 +0530

    Fixed auth route

--- client/Weather Application/src/helper/ProtectedRoutes.jsx
+++ client/Weather Application/src/helper/ProtectedRoutes.jsx
@@ -2,7 +2,7 @@ import {Navigate} from 'react-router-dom'
 import {useAuth} from './AuthContext.jsx'
 
 export default function ProtectedRoute({children}){
-  const {token} = useAuth
+  const {token} = useAuth()
 
   //Check if the token exist if it does not exist send then back to the login screen
   if(!token){
