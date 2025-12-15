6fee6eb6427efaee6e97a1bfcca262a4f96447a4
kuraohikari123 <patrick@patrick.localdomain>
2025-12-15 22:08:58 +0530

    Corrected auth spelling

--- client/Weather Application/src/main.jsx
+++ client/Weather Application/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {AuthProvider }from './helper/AuthContext.jsx'
 
+//need to replace the strict mode with the authprovider for safe login
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <AuthProvider>
     <App />
-  </StrictMode>,
+  </AuthProvider>,
 )
