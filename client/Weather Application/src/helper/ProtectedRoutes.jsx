631971a5a806011d834d12a25bff513cd3cbf0ac
kuraohikari123 <patrick@patrick.localdomain>
2025-12-19 00:50:20 +0530

    Fixed protected routes rendering early

--- client/Weather Application/src/helper/ProtectedRoutes.jsx
+++ client/Weather Application/src/helper/ProtectedRoutes.jsx
@@ -2,7 +2,9 @@ import {Navigate} from 'react-router-dom'
 import {useAuth} from './AuthContext.jsx'
 
 export default function ProtectedRoute({children}){
-  const {token} = useAuth()
+  const {token,loading} = useAuth()
+
+  if(loading) return <div>Loading...</div>
 
   //Check if the token exist if it does not exist send then back to the login screen
   if(!token){
