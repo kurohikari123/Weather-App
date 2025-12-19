09e330de79d52ea20f3220ffdbd86eaf08f538b4
kuraohikari123 <patrick@patrick.localdomain>
2025-12-20 00:19:42 +0530

    Added register end point

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,16 +4,48 @@ import express from "express"
 import pool from '../database/db.js'
 import 'dotenv/config'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 const router = express.Router()
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const JWT_SECRET = process.env.JWT_SECRET
+const SALT = 10
+
+//REGISTER ROUTE
+router.post('/register',async (req,res)=>{
+
+   const email = req.body.email
+   const name = req.body.name
+   const password = req.body.password
+
+   const sql = "INSERT into user(email,name,password) values(?,?,?)"
+
+   //I need to encrypt the password
+   const e_password = await bcrypt.hash(password,SALT)
+   try{
+   const [sql_result] = await pool.query(sql,[email,name,e_password])
+   
+   if(sql_result.affectedRows === 1){
+      res.status(200).json({'status':1,'message':'Thank you for registering!'})
+    }
+    else{
+      res.status(400).json({'status':3,'message':'Something went wrong!'})
+    }
+  }
+  catch(e){
+    res.status(400).json({'status':3,'message':'Internal server Error!'})
+  }
+
+})
 
 //Login Route
 router.post('/login',async (req,res)=>{
 
   const email = req.body.email
   const password = req.body.password
+
+  //Need to do this and then compare hashed passwords
+  const e_password = await bcrypt.hash(password,SALT)
 
   try{   
 
@@ -22,10 +54,10 @@ router.post('/login',async (req,res)=>{
     res.status(400).json({"status":0,"message":"Invalid email"})
   }
 
-  //Need to getch name as well
+  //Need to get name as well
   const sql = 'select * from user where email = ? '
   const row = await pool.query(sql,[email])
-  console.log(row[0][0].name)
+  console.log(row[0][0].password)
 
   const name = row[0][0].name
 
@@ -36,7 +68,7 @@ router.post('/login',async (req,res)=>{
 
   //Test password
   //Also need to set token for verification (JWT)
-  if(row[0][0].password == password){
+  if(row[0][0].password == e_password){
 
     //Create a JWT payload
     const payload = {
